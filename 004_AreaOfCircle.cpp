0c7294a6a8f3d3b9cdc28fd4b7861fd8f08d6f77
Zainab Gondal <gondalzainab34@gmail.com>
2026-01-13 01:33:21 +0500

    Create 004_AreaOfCircle.cpp

---
+++ 004_AreaOfCircle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+	//Area of circle
+	float r;
+	cout<<"put the value of radius: ";
+	cin>>r;
+	
+	float area = 3.141592*r*r;
+	cout<<"The area of a circle is: "<<area;
+	return 0;
+}

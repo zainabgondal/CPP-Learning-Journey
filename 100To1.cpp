a0a46a8e486a559feb88fd00543b68eadb61f45c
Zainab Gondal <gondalzainab34@gmail.com>
2026-01-13 01:13:47 +0500

    Implement countdown from 100 to 1 in 100To1.cpp

---
+++ 100To1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	for(int i=100;i>=1;i--) // 100 to 1
+//  for(int i=100;i>=1;i=i-2) 100,98,96,94,92........2
+//   for(int i=100;i>=1;i=i/2)  100,50,25,12,6,3,1
+	{
+		cout<<i<<" ";
+	}
+}

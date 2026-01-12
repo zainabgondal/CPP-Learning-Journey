1175e3dac78c5dac4aa1fdd7f850394dffcbc5d5
Zainab Gondal <gondalzainab34@gmail.com>
2026-01-13 01:25:14 +0500

    Add example of address-of operator in C++
    Demonstrate the use of the address-of operator to print the memory address of a variable.

---
+++ 002_AddressOfVariable.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x = 3;
+    cout<<&x<<endl;
+ 
+    //adding & will show us the address of variable in RAM.(hexa decimal address) 
+    
+
+}

0cd922f7b02ac47292dd0d485403463d17bbefcd
Zainab Gondal <gondalzainab34@gmail.com>
2026-01-13 01:26:02 +0500

    Add variable naming rules as comments

---
+++ 003_VariableNamingRules.cpp
@@ -0,0 +1,5 @@
+// variable naming rules:
+// 	1. Variables can start from an alphabet or underscore _ or $.
+// 	2. Special characters except _ and $ are not allowed.
+// 	3. Some particular keywords are not allowed.
+// 	4. Commas or blanks are not allowed.
